582010df1b5cdb040f7d45ab6c6b8f7df6982d48
shery15 <382254994@qq.com>
2017-04-12 23:37:26 +0800

    add apm.sh

---
+++ apm.sh
@@ -0,0 +1,32 @@
+# !/bin/bash
+
+# Awesome Atom Packages
+
+# Here's a list of several atom-packages for Web developers.
+# With these atom-packages , atom will provide IDE-like functionality.
+
+apm install \
+language-babel \
+language-postcss \
+autocomplete-modules \
+autocomplete-paths \
+atom-ternjs \
+emmet \
+linter \
+linter-eslint \
+editorconfig \
+git-plus \
+merge-conflicts \
+project-manager \
+minimap \
+file-icons \
+atom-beautify \
+pigments \
+color-picker \
+highlight-selected \
+minimap-highlight-selected \
+docblockr \
+markdown-preview-plus \
+hyperclick \
+js-hyperclick \
+travis-ci-status \
